3db9260923ecc5b33b8da120595ef4c10e87ea14
Harini <137061073+HariniS-311@users.noreply.github.com>
2025-06-23 12:20:01 +0530

    Update Rem_dup.java

--- Rem_dup.java
+++ Rem_dup.java
@@ -20,7 +20,27 @@ System.out.print(arr[i]+"");
 }
 }
 
+ANOTHER METHOD
 
+import java.util.*;
+  class Main{
+    public static void main(String[] args){
+      int[] arr = {1,2,2,3,4,4,5};
+   for(int i=0;i<arr.length;i++)
+{
+  int j=0;
+  for(j=0;j<i;j++){
+if(arr[i]==arr[j]){
+  break;
+}
+}
+    if(i==j){
+ System.out.print(arr[i]+" ");
+}
+}
+}
+}
+  
 
 
 
